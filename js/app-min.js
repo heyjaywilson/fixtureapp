2c55d4944049df1c6bd88a8568ad9693c1431728
maeganjwilson <maegan@maeganwilson.com>
2017-05-19 17:09:18 -0500

    Need to work on getting fixture universe number

--- js/app-min.js
+++ js/app-min.js
@@ -1,7 +1,18 @@
 /* eslint-env jquery*/
+function calculateSU(uni){
+    var subnet = Math.floor(uni/16);
+    var universe = uni - (subnet * 16);
+    return 'ArtNet subnet: ' + subnet + '\nArtNet universe: '+universe;
+}
+
+function renderFixtAnswer(universe, elm){
+    elm.html('<strong>'+calculateSU(universe)+'</strong>');
+}
 $('#fixture-form').submit(function() {
-    var fixtUni = $('#fixtUni').val();
-    console.log(fixtUni);
+    event.preventDefault();
+    var fixt = $('#fixtUni').val();
+    $('#fixt-answer').show();
+    renderFixtAnswer(fixt, $('#fixt-answer'));
 });
 
 
--- js/app.js
+++ js/app.js
@@ -2,14 +2,15 @@
 function calculateSU(uni){
     var subnet = Math.floor(uni/16);
     var universe = uni - (subnet * 16);
-    return 'ArtNet subnet:' + subnet + '\nArtNet universe: '+universe;
+    return 'ArtNet subnet: ' + subnet + '\nArtNet universe: '+universe;
 }
 
 function renderFixtAnswer(universe, elm){
-    elm.html(calculateSU(universe));
+    elm.html('<strong>'+calculateSU(universe)+'</strong>');
 }
 $('#fixture-form').submit(function() {
     event.preventDefault();
     var fixt = $('#fixtUni').val();
+    $('#fixt-answer').show();
     renderFixtAnswer(fixt, $('#fixt-answer'));
 });
